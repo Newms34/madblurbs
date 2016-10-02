c1c87b175238314e445893aea4573937c64be4bb
newms34 <newms3450@gmail.com>
2016-10-02 19:04:01 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+#Mad-Blurbs
+
+##Contents:
+ - [What it is](#what-it-is)
+ - [How to use it](#how-to-use-it)
+ - [How it works](#how-it-works)
+ - [Credits](#credits)
+
+##What it is
+
+Mad-Blurbs is a small experiment in auto-generated but grammatically-correct sentences. It's basically Mad-libs, but the computer fills in the blanks for you.
+
+##How to use it
+
+There are three categories of elements you can add. 
+
+- The first (and most complicated) is Words. Here, you pick a general category, and then the specific subtype. For example, let's say you want a sentence identical to the following, grammatically: `The dog barks`. You'd pick Verb, and then 3rd sg. Present for the verb type.
+
+- The second is Fragments. These are basically equivalent to the words between the blanks in regular Mad-libs. To use the example from above, let's say you want to make sure your sentence always starts with the word "the". You'd write 'the' as a fragment
+
+- The final category is Punctuation. Pretty self-explanatory: if you need punctuation, go ahead and add it.
+
+Once you've added all your elements, rearrange them using the Arrangement box. Just click the left or right arrows to change an element's position.
+
+Then click 'Do it!'
+
+##How it works
+
+I'm basically using wiktionary's api to fetch word categories. For example, if I want a present participle, I send a get request to [this address](https://en.wiktionary.org/w/api.php?action=query&list=categorymembers&cmlimit=500&cmnamespace=0&format=json&cmtitle=Category:English_present_participles).
+
+I then join everything in the order shown in the Arrangement box.
+
+Oh, and AngularJS.
+
+##Credits:
+ - Me, [David Newman](https://github.com/Newms34)
+ - Wiktionary's API, [Wiktionary.org](https://en.wiktionary.org/w/api.php?action=help)
+
---
+++ js/main.js
@@ -0,0 +1,224 @@
+Array.prototype.move = function(old_index, new_index) {
+    if (new_index >= this.length) {
+        var k = new_index - this.length;
+        while ((k--) + 1) {
+            this.push(undefined);
+        }
+    }
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+    return this; // for testing purposes
+}
+var app = angular.module('ml', []).controller('ml-con', function($scope, $q, $http) {
+    $scope.words = [];
+    $scope.currWrdType = null;
+    $scope.frags = [];
+    $scope.puncs=[];
+    $scope.puncTypes = ['.',',','!','?',';']
+    $scope.addPunc=false;
+    $scope.currWrdSt = null;
+    $scope.uriBase = 'https://en.wiktionary.org/w/api.php?action=query&list=categorymembers&cmlimit=500&cmnamespace=0&format=json&cmtitle=Category:'
+    $scope.wrdConst = function(t, st, u) {
+        this.type = t;
+        this.subtype = st;
+        this.uri = u;
+        this.val = null;
+    }
+    $scope.wordTypes = [{
+        type: 'Noun',
+        subtypes: [{
+            type: 'Countable Noun',
+            ex: 'Dog',
+            actualType: 'English_countable_nouns'
+        }, {
+            type: 'Uncountable Noun',
+            ex: 'Sheep',
+            actualType: 'English_uncountable_nouns'
+        },{
+            type: 'Plural Noun',
+            ex: 'Cats',
+            actualType: 'English_noun_plural_forms'
+        }, {
+            type: 'Proper Noun',
+            ex: 'England',
+            actualType: 'English_proper_nouns'
+        }, {
+            type: 'First (given) Name, Male',
+            ex: 'John',
+            actualType: 'English_male_given_names'
+        }, {
+            type: 'First (given) Name, Female',
+            ex: 'Betty',
+            actualType: 'English_female_given_names'
+        }, {
+            type: 'Surname',
+            ex: 'Smith',
+            actualType: 'English_surnames'
+        }]
+    }, {
+        type: 'Adjective',
+        subtypes: [{
+            type: 'Positive (Simple)',
+            ex: 'Big',
+            actualType: 'English_adjectives'
+        }, {
+            type: 'Comparative',
+            ex: 'Louder',
+            actualType: 'English_adjective_comparative_forms'
+        }, {
+            type: 'Superlative',
+            ex: 'Smallest',
+            actualType: 'English_adjective_superlative_forms'
+        }]
+    }, {
+        type: 'Verb',
+        subtypes: [{
+            type: 'Past Participle',
+            ex: 'Risen',
+            actualType: 'English_past_participles'
+        }, {
+            type: 'Present Transitive',
+            ex: 'Demonstrate',
+            actualType: 'English_transitive_verbs'
+        }, {
+            type: 'Present Intransitive',
+            ex: 'Exist',
+            actualType: 'English_intransitive_verbs'
+        }, {
+            type: 'Present Participle',
+            ex: 'Eating',
+            actualType: 'English_present_participles'
+        }, {
+            type: '3rd sg. Present',
+            ex: 'Thinks',
+            actualType: 'English_third-person_singular_forms'
+        }, {
+            type: 'Simple Past',
+            ex: 'Debated',
+            actualType: 'English_verb_simple_past_forms'
+        }]
+    }, {
+        type: 'Adverb',
+        subtypes: [{
+            type: 'Conjunctive Adverbs',
+            ex: 'However',
+            actualType: 'English_conjunctive_adverbs'
+        }, {
+            type: 'Regular Adverbs',
+            ex: 'Quickly',
+            actualType: 'English_adverbs'
+        }]
+    }];
+    $scope.getObj = function(a, w) {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i].type == w) {
+                return a[i];
+            }
+        }
+        return false;
+    }
+    $scope.moveLeft = function(n) {
+    	console.log(n)
+        $('#blurb-order').fadeOut(200, function() {
+            if (n && n !== 0) {
+                $scope.allBlurbs.move(n, n - 1);
+            } else {
+                $scope.allBlurbs.move(n, $scope.allBlurbs.length - 1)
+            }
+            $scope.$digest();
+            $('#blurb-order').fadeIn(200)
+        });
+
+    };
+    $scope.moveRight = function(n) {
+        $('#blurb-order').fadeOut(200, function() {
+            if (n < $scope.allBlurbs.length - 1) {
+                $scope.allBlurbs.move(n, n + 1);
+            } else {
+                $scope.allBlurbs.move(n, 0)
+            }
+            $scope.$digest();
+            $('#blurb-order').fadeIn(200)
+        });
+    };
+    $scope.allBlurbs = [];
+    $scope.doBlurbs = function() {
+        $scope.allBlurbs = [];
+        $scope.allBlurbs = $scope.allBlurbs.concat($scope.words).concat($scope.frags).concat($scope.puncs);
+    }
+    $scope.addWrd = function() {
+        $scope.words.push(new $scope.wrdConst($scope.currWrdType, $scope.currWrdSt.type, $scope.currWrdSt.actualType));
+        $scope.pickinWord = false;
+        $scope.doBlurbs();
+    };
+    $scope.addPunctuation = function(){
+    	$scope.puncs.push($scope.currPunc);
+    	$scope.addPunc=false;
+    	$scope.doBlurbs();
+    }
+    $scope.addFragment = function() {
+        $scope.frags.push($scope.currFrag)
+        $scope.addFrag = false;
+        $scope.doBlurbs();
+    };
+    $scope.removeWord = function(n) {
+        $scope.words.splice(n, 1);
+        $scope.doBlurbs();
+    };
+    $scope.removeFrag = function(n) {
+        $scope.frags.splice(n, 1);
+        $scope.doBlurbs();
+    };
+    $scope.alliterate = false;
+    $scope.autocap = true;
+        $scope.fullStr = 'The quick brown fox jumps over the lazy dog';
+    $scope.makeSentence = function() {
+        console.log($scope.allBlurbs);
+        var wrdProms = [];
+        var whichLet = '&cmsort=sortkey&cmstartsortkeyprefix='+String.fromCharCode(Math.floor(Math.random()*26)+65);//in case of alliterating!
+        for (var i = 0; i < $scope.allBlurbs.length; i++) {
+            if (typeof $scope.allBlurbs[i] != 'string') {
+                // $.ajax({method:'GET',url:})
+                if (!$scope.alliterate) whichLet = '&cmsort=sortkey&cmstartsortkeyprefix='+String.fromCharCode(Math.floor(Math.random()*26)+65);
+                wrdProms.push(
+                    $.ajax({
+                        method: 'GET',
+                        url: $scope.uriBase + $scope.allBlurbs[i].uri+whichLet,
+                        dataType: 'jsonp'
+                    })
+                );
+            }
+        }
+        $q.all(wrdProms).then(function(r) {
+            console.log(r);
+            $scope.fullStr='';
+            var currRemoteWrd=0;
+            for(i=0;i<$scope.allBlurbs.length;i++){
+            	var newBit;
+            	if (typeof $scope.allBlurbs[i] == 'string') {
+            		newBit=$scope.allBlurbs[i]+' ';
+            	}else{
+            		newBit=r[currRemoteWrd].query.categorymembers[Math.floor(Math.random()*r[currRemoteWrd].query.categorymembers.length)].title+' ';
+            		currRemoteWrd++;
+            	}
+            	if($scope.autocap){
+            		//auto-capitalization
+            		if (i==0 || ($scope.fullStr[$scope.fullStr.length-2].match(/\!|.|\,|\?|\;/).length>1)){
+            			newBit = newBit[0].toUpperCase()+newBit.slice(1);
+            		}
+            	}
+            	$scope.fullStr+=newBit;
+            }
+        })
+    }
+    $scope.clearIt = function(){
+    	bootbox.confirm('Are you sure you wanna clear everything (Words, Fragments, and Punctuation)?',function(r){
+    		if (r && r!=null){
+    			$scope.allBlurbs = [];
+    			$scope.words = [];
+    			$scope.frags = [];
+    			$scope.puncs = [];
+    			$scope.$digest();
+    		}
+    	})
+    }
+});
